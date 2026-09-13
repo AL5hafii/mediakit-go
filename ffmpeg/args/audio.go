e717c8f38e46f0565056ef3d05020878ad0b0faf
AlShafii <Al5hafii@proton.me>
2026-09-13 17:32:56 +0300

    [mod] ffmpegArgs - video & audio

--- ffmpeg/args/audio.go
+++ ffmpeg/args/audio.go
@@ -1,9 +1,9 @@
 package ffmpegArgs
 
 type Audio struct {
-	Codec,
-	Channels,
-	SampleRate string
+	Codec      string // -c:a codec is the audio codec to use (e.g., "aac", "mp3", etc.)
+	Channels   string // -ac value is the number of audio channels (e.g., "2" for stereo)
+	SampleRate string // -ar value is the audio sample rate (e.g., "44100" for 44.1 kHz)
 }
 
 func (st Audio) Args() Args {
@@ -12,6 +12,10 @@ func (st Audio) Args() Args {
 	args.Arg("-c:a", st.Codec)
 	args.Arg("-ac", st.Channels)
 	args.Arg("-ar", st.SampleRate)
+
+	if len(args) == 0 {
+		return nil
+	}
 
 	return args
 }
--- ffmpeg/args/video.go
+++ ffmpeg/args/video.go
@@ -1,13 +1,27 @@
 package ffmpegArgs
 
 type Video struct {
-	Codec string
+	Crf       string // -crf value is the Constant Rate Factor for controlling quality (e.g., "23" for good quality)
+	Codec     string // -c:v codec is the video codec to use (e.g., "libx264", "libvpx", etc.)
+	Aspect    string // -aspect value is the aspect ratio for the video stream (e.g., "16:9" for widescreen)
+	Bitrate   string // -b:v value is the target bitrate for the video stream (e.g., "1000k" for 1000 kbps)
+	Framerate string // -r value is the frame rate for the video stream (e.g., "30" for 30 fps)
+	FrameSize string // -s value is the frame size for the video stream (e.g., "1920x1080" for 1080p)
 }
 
 func (st Video) Args() Args {
 	args := make(Args, 0)
 
+	args.Arg("-crf", st.Crf)
 	args.Arg("-c:v", st.Codec)
+	args.Arg("-aspect", st.Aspect)
+	args.Arg("-b:v", st.Bitrate)
+	args.Arg("-r", st.Framerate)
+	args.Arg("-s", st.FrameSize)
+
+	if len(args) == 0 {
+		return nil
+	}
 
 	return args
 }
